e37f220e5d5528ce7ea59950f51bdfb17cba442b
Damian <damian.thoma@student.htldornbirn.at>
2025-02-27 09:25:46 +0100

    Games_Tutorial01/Tutorial02_watched

---
+++ src/at/damian/games/firstGame/Rectangles.java
@@ -0,0 +1,52 @@
+package at.damian.games.firstGame;
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.tests.AnimationTest;
+
+public class Rectangles extends BasicGame {
+    private float x;
+    private float y;
+    private float speed;
+
+
+    public Rectangles(String title) {
+        super(title);
+    }
+
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+        this.x = 100;
+        this.y = 100;
+        this.speed = 3;
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int deltaTime) throws SlickException {
+        this.x += (float) deltaTime /this.speed;
+        this.y+= (float) deltaTime /4;
+
+        if (this.x == 800 - 100 || this.y == 600-100) {
+            this.x = 0;
+            this.y = 0;
+        }
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+        graphics.drawRect(this.x, y, 100, 100);
+        graphics.drawString("Hello", 50, 100);
+
+    }
+
+    public static void main(String[] argv) {
+        try {
+            AppGameContainer container = new AppGameContainer(new Rectangles("Rectangles"));
+            container.setDisplayMode(800,600,false);
+            container.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+    }
+}
