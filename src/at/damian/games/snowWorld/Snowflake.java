22c76af017bb6813439b8f74b3e6221b0ed2c254
Damian <damian.thoma@student.htldornbirn.at>
2025-03-27 09:28:07 +0100

    Games_SnowWorld_progression

---
+++ src/at/damian/games/snowWorld/Snowflake.java
@@ -0,0 +1,24 @@
+package at.damian.games.snowWorld;
+
+import org.newdawn.slick.Graphics;
+
+import java.util.Random;
+
+public class Snowflake implements Actor {
+    public enum SIZE {SMALL,MEDIUM,LARGE}
+    private float x,y;
+    private int size;
+    private int speed;
+    private Random random;
+
+
+    @Override
+    public void render(Graphics graphics) {
+
+    }
+
+    @Override
+    public void update(int delta) {
+
+    }
+}
